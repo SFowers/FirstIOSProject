aa076c651b36829b26b9e3985ffbddb0c94367c9
Sean Fowers <sean.fowers@griffithuni.edu.au>
2023-03-09 13:08:32 +1000

    Initial Commit

---
+++ FirstIOSProject/FirstIOSProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstIOSProjectApp.swift
+//  FirstIOSProject
+//
+//  Created by Sean Fowers on 9/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct FirstIOSProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
